2e7d01a65ae84b0bbee6b5915c51403a7090ce4d
PRATHEEP RAJ S <PRATHEEP RAJ S@LAPTOP-IAG9IU3M>
2025-12-30 13:37:00 +0530

    new

--- src/test/java/organization/Sample2.java
+++ src/test/java/organization/Sample2.java
@@ -3,6 +3,7 @@ package organization;
 public class Sample2 {
 public static void main(String[] args) {
 	System.out.println("heyyy");
+	System.out.println("30-12-2025");
 }
 	// TODO Auto-generated method stub
 
